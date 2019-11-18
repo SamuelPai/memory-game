b6494e727ee95929184e21e5b5ccab4eab7162cd
Samuel Pai <samuelpai16@gmail.com>
2019-11-17 23:57:41 -0800

    Updates

---
+++ precache-manifest.a674ac374b2b96014c5dcde6e5f7f8eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3f13c85067c667d71e7033ab65fffc",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "72009097c51fbe70bddd",
+    "url": "/memory-game/static/css/main.53766e58.chunk.css"
+  },
+  {
+    "revision": "93d7c0073a6766b07ab2",
+    "url": "/memory-game/static/js/2.dae3e278.chunk.js"
+  },
+  {
+    "revision": "72009097c51fbe70bddd",
+    "url": "/memory-game/static/js/main.3267721f.chunk.js"
+  },
+  {
+    "revision": "3c6c1d3098f1f9b94d8d",
+    "url": "/memory-game/static/js/runtime-main.53e70fbf.js"
+  }
+]);
